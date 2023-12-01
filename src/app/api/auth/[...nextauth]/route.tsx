d5d9244bef757a60e26ac98c54ace674079cb969
Leon Ampah <l.ampah@akofisgroup.com>
2023-12-01 14:48:49 +0000

    Removed export from auth options

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -7,7 +7,7 @@ import { Adapter } from 'next-auth/adapters'
 import NextAuth from 'next-auth/next'
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: NextAuthOptions = {
+ const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GoogleProvider({
